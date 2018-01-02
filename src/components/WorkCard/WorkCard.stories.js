b0393d5fa0dcc8ad739b1038bde79527a34801be
Jonathan Blair <jon@tothemun.com>
2018-01-02 14:28:41 -0500

    Added test data for wordcard.stories.js

--- src/components/WorkCard/WorkCard.stories.js
+++ src/components/WorkCard/WorkCard.stories.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { storiesOf  } from '@storybook/react';
 import { Container, Col, Row } from 'react-grid-system';
+import testData from './testData';
 import WorkCard from './WorkCard';
 
 storiesOf('WorkCard', module)
@@ -9,11 +10,10 @@ storiesOf('WorkCard', module)
       <Row>
         <Col xs={6}>
           <WorkCard
-            imgSrc='https://images.unsplash.com/photo-1500043189552-8feddf8d9f64?auto=format&fit=crop&w=1934&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D'
-            title='On Writing and Logs'
             loaded={true}
+            page={testData}
           />
         </Col>
       </Row>
     </Container>
-  ))
+  ));
