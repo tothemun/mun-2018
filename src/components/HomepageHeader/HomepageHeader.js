ced94d008172a68675592e2483511a81d50e4e91
Jonathan Blair <jon@tothemun.com>
2018-03-19 13:29:36 -0400

    Converted Homepage header to component

--- src/components/HomepageHeader/HomepageHeader.js
+++ src/components/HomepageHeader/HomepageHeader.js
@@ -1,23 +1,30 @@
 import cn from 'classnames';
-import React from 'react';
+import React, { Component } from 'react';
+import React3 from 'react-three-renderer';
 import PropTypes from 'prop-types';
 import { ProgressiveImage } from '_components';
 import styles from './HomepageHeader.css';
 import HeaderImg from './header-img.jpg';
 import Headline from './headline.svg';
 
-const HomepageHeader = ({ alt, className, display, src }) => (
-  <div className={styles.header}>
-    <div className={cn(styles.image, {[styles.display]: display})}>
-      <span className={styles.left}></span>
-      <span className={styles.top}></span>
-      <span className={styles.right}></span>
-      <span className={styles.bottom}></span>
-      <ProgressiveImage src={HeaderImg} alt='Header' />
-      <object data={Headline} alt='Make It Matter' aria-label='Make It Matter' className={styles.headline}/>
-    </div>
-  </div>
-);
+// const HomepageHeader = ({ alt, className, display, src }) => (
+//   <div className={styles.header}>
+//     <div className={cn(styles.image, {[styles.display]: display})}>
+//       <span className={styles.left}></span>
+//       <span className={styles.top}></span>
+//       <span className={styles.right}></span>
+//       <span className={styles.bottom}></span>
+//       <ProgressiveImage src={HeaderImg} alt='Header' />
+//       <object data={Headline} alt='Make It Matter' aria-label='Make It Matter' className={styles.headline}/>
+//     </div>
+//   </div>
+// );
+
+class HomepageHeader extends Component {
+  render9) {
+    
+  }
+}
 
 HomepageHeader.propTypes = {
   className: PropTypes.string
