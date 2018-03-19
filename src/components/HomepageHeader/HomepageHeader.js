140d6c9ab08e7be4ea1ae51e490574529d42fd51
Jonathan Blair <jon@tothemun.com>
2018-03-19 15:13:05 -0400

    Added three renderer

--- src/components/HomepageHeader/HomepageHeader.js
+++ src/components/HomepageHeader/HomepageHeader.js
@@ -1,33 +1,67 @@
-import cn from 'classnames';
 import React, { Component } from 'react';
 import React3 from 'react-three-renderer';
-import PropTypes from 'prop-types';
-import { ProgressiveImage } from '_components';
-import styles from './HomepageHeader.css';
-import HeaderImg from './header-img.jpg';
-import Headline from './headline.svg';
-
-// const HomepageHeader = ({ alt, className, display, src }) => (
-//   <div className={styles.header}>
-//     <div className={cn(styles.image, {[styles.display]: display})}>
-//       <span className={styles.left}></span>
-//       <span className={styles.top}></span>
-//       <span className={styles.right}></span>
-//       <span className={styles.bottom}></span>
-//       <ProgressiveImage src={HeaderImg} alt='Header' />
-//       <object data={Headline} alt='Make It Matter' aria-label='Make It Matter' className={styles.headline}/>
-//     </div>
-//   </div>
-// );
+import * as THREE from 'three';
 
 class HomepageHeader extends Component {
-  render9) {
-    
-  }
-}
+  constructor(props, context) {
+    super(props, context);
 
-HomepageHeader.propTypes = {
-  className: PropTypes.string
-};
+    // construct the position vector here, because if we use 'new' within render,
+    // React will think that things have changed when they have not.
+    this.cameraPosition = new THREE.Vector3(0, 0, 5);
+  }
+
+  onAnimate = () => {
+    this.setState({
+      cubeRotation: new THREE.Euler(
+        this.state.cubeRotation.x + 0.1,
+        this.state.cubeRotation.y + 0.1,
+        0
+      ),
+    });
+  };
+
+
+  render() {
+    const width = window.innerWidth; // canvas width
+    const height = window.innerHeight; // canvas height
+
+    return (<React3
+      mainCamera="camera" // this points to the perspectiveCamera which has the name set to "camera" below
+      width={width}
+      height={height}
+
+      onAnimate={this.onAnimate}
+    >
+      <scene>
+        <perspectiveCamera
+          name="camera"
+          fov={75}
+          aspect={width / height}
+          near={0.1}
+          far={1000}
+
+          position={this.cameraPosition}
+        />
+        <mesh
+          rotation={this.state.cubeRotation}
+        >
+          <boxGeometry
+            width={1}
+            height={1}
+            depth={1}
+          />
+          <meshBasicMaterial
+            color={0x00ff00}
+          />
+        </mesh>
+      </scene>
+    </React3>);
+  }
+
+  state = {
+    cubeRotation: new THREE.Euler(),
+  };
+}
 
 export default HomepageHeader;
