6cc366378b2af2b0503cd7f2a9e5ae2d70246aec
Jonathan Blair <jon@tothemun.com>
2018-03-19 16:27:52 -0400

    Made 3d sphere background

--- src/components/HomepageHeader/HomepageHeader.js
+++ src/components/HomepageHeader/HomepageHeader.js
@@ -2,9 +2,8 @@ import cn from 'classnames';
 import React, { Component } from 'react';
 import React3 from 'react-three-renderer';
 import * as THREE from 'three';
-import { ProgressiveImage } from '_components';
+import Sphere from './Sphere';
 import styles from './HomepageHeader.css';
-import HeaderImg from './header-img.jpg';
 import Headline from './headline.svg';
 
 class HomepageHeader extends Component {
@@ -12,34 +11,15 @@ class HomepageHeader extends Component {
     this.cameraPosition = new THREE.Vector3(0, 0, 5);
   }
 
-  onMouseMove = (e) => {
-    const { oldMouseX, oldMouseY } = this.state;
-
-    this.setState({
-      difMouseX: e.screenX - oldMouseX,
-      difMouseY: e.screenY - oldMouseY,
-      oldMouseX: e.screenX,
-      oldMouseY: e.screenY
-    });
-  }
-
   onAnimate = () => {
-    const { difMouseX, difMouseY } = this.state;
-    const mouseMod = 1000;
-
-    this.setState({
-      sphereRotation: new THREE.Euler(
-        this.state.sphereRotation.x + 0.01 + difMouseY / mouseMod,
-        this.state.sphereRotation.y + 0.01 + difMouseX / mouseMod,
-        0
-      ),
-    });
-  };
+    this.$sphere.onAnimate();
+  }
 
   render() {
     const width = window.innerWidth; // canvas width
     const height = window.innerHeight; // canvas height
     const { display } = this.props;
+
     return (
       <div className={styles.header} onMouseMove={this.onMouseMove}>
         <div
@@ -50,9 +30,9 @@ class HomepageHeader extends Component {
           <span className={styles.top}></span>
           <span className={styles.right}></span>
           <span className={styles.bottom}></span>
-          <ProgressiveImage src={HeaderImg} alt='Header' />
           <React3
             alpha={true}
+            antialias={true}
             mainCamera="camera"
             width={width}
             height={height}
@@ -68,16 +48,14 @@ class HomepageHeader extends Component {
                 far={1000}
                 position={this.cameraPosition}
               />
-              <mesh rotation={this.state.sphereRotation} >
-                <icosahedronGeometry
-                  radius={1}
-                  detail={1}
+              <ambientLight color={0x353535} />
+              <directionalLight
+                color={0xFFFFFF}
+                position={new THREE.Vector3(15, 15, 15)}
+                lookAt={new THREE.Vector3(0, 0, 0)}
+                intensity={.5}
                 />
-                <meshBasicMaterial
-                  color={0xAAAAAAA}
-                  wireframe={true}
-                />
-              </mesh>
+              <Sphere ref={ (el) => this.$sphere = el}/>
             </scene>
           </React3>
           <object data={Headline} alt='Make It Matter' aria-label='Make It Matter' className={styles.headline}/>
@@ -85,14 +63,6 @@ class HomepageHeader extends Component {
       </div>
     );
   }
-
-  state = {
-    oldMouseX: 0,
-    oldMouseY: 0,
-    difMouseX: 0,
-    difMouseY: 0,
-    sphereRotation: new THREE.Euler(),
-  };
 }
 
 export default HomepageHeader;
